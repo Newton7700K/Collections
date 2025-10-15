b5cf80dc13340991d08b10b4b242eea95596780f
Newton Zheng <newtonzheng2009@gmail.com>
2025-10-15 13:17:48 -0700

    Constructor, push, pop, toString

--- MyStack.java
+++ MyStack.java
@@ -1,33 +1,40 @@
-
-/**
- * Write a description of class MyStack here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class MyStack
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyStack
-     */
-    public MyStack()
-    {
-        // initialise instance variables
-        x = 0;
+    /*
+        - public MyStack() – Constructs a stack that can hold 100 elements.
+        - public MyStack(int maxSize) – Constructs a stack that can hold max.
+        - public void push(int element) – Pushes an element on to the stack.
+        - public int pop() – Pops an element off of the stack.
+        - public boolean isEmpty() – Indicates whether stack contains any elements.
+        - public int top() – Reads the element at the top of the stack.
+        - public int size() – Returns the number of elements stored in the stack.
+        - public boolean isFull() – Indicates whether the stack has exhausted its available storage.
+        - public String toString() – Returns the contents of the stack from top to bottom.
+    */
+   
+    private int[] stack;
+    private int top = 0;
+    
+    public MyStack(int maxSize){
+        stack = new int[maxSize];
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public void push(int element){
+        stack[top] = element;
+        top++;
     }
+    
+    public int pop(){
+        top--;
+        return stack[top+1];
+    }
+    
+    public String toString(){
+        String str = stack[0] + "";
+        for(int i = 1; i<top; i++){
+            str += ", " + stack[i];
+        }
+        return str;
+    }
+    
 }
