94782100ae0f85d904f4fad1843006797f6ca433
Newton Zheng <newtonzheng2009@gmail.com>
2025-10-31 09:11:30 -0700

    Created MyLinkedList

---
+++ MyLinkedList.java
@@ -0,0 +1,84 @@
+public class MyLinkedList<E>
+{
+    
+    private Node<E> head;
+    private Node<E> tail;
+    
+        
+    public MyLinkedList(E value) {
+        addHead(value);
+        tail = head;
+    }
+    
+    public void addTail(E element) {
+        Node currentNode = new Node(element, tail);
+        tail.setNext(currentNode);
+        tail = currentNode;
+    }
+    
+    public void addHead(E element) {
+        Node currentNode = new Node(element, head);
+        head = currentNode;
+    }
+    
+    public E removeHead(){
+        E element = head.getElement();
+        head = head;
+        return element;
+    }
+    
+    public E getHead(){
+        return head.getElement();
+    }
+    
+    public boolean isEmpty(){
+        return head == null && tail == null;
+    }
+    
+    public int size(){
+        Node<E> currentNode = head;
+        int count = 0;
+        while(currentNode != null){
+            count++;
+            currentNode = currentNode.getNext();
+        }
+        return count;
+    }
+    
+    public String toString(){
+        Node<E> currentNode = head;
+        String str = "";
+        if(currentNode != null){
+            str += currentNode.getElement();
+        }
+        while(currentNode != null){
+            str += ", " + currentNode.getElement();
+            currentNode = currentNode.getNext();
+        }
+        return str;
+    }
+   
+   public class Node<E>
+    {
+        private E element;
+        private Node next;
+        
+        public Node(E value, Node next) {
+            this.element = value;
+            this.next = next;
+        }
+        
+        public E getElement() {
+            return element;
+        }
+        
+        public Node getNext(){
+            return next;
+        }
+        
+        public void setNext(Node next){
+            this.next = next;
+        }
+    }
+    
+}
