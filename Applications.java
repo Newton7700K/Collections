2e6e3d3f52230258fc286140d04297e95b780d45
Newton Zheng <newtonzheng2009@gmail.com>
2025-11-12 13:58:54 -0800

    Applications

--- Applications.java
+++ Applications.java
@@ -12,10 +12,20 @@ public class Applications
     }
     
     public static boolean checkSyntax(String code){
-        
+        MyStack<String> stack = new MyStack<String>(code.length());
+        for(int i = 0; i<code.length(); i++){
+            if ("<{(".indexOf(code.substring(i,i+1)) != -1) {
+                stack.push(code.substring(i,i+1));
+            } else if (">})".indexOf(code.substring(i,i+1)) != -1) {
+                if(stack.isEmpty() || (">})".indexOf(code.substring(i,i+1)) != "<{(".indexOf(stack.top()))){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
     }
     
     public static double evalPostfix(String expression){
-        
+        return  0.0;
     }
 }
