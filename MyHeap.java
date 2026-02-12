c7ac31bbe7aa5cad5af6f5e56dac523f59f253ae
Newton Zheng <newtonzheng2009@gmail.com>
2026-02-12 08:57:25 -0800

    Changes

--- MyHeap.java
+++ MyHeap.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.rrays;
 
 /**
  * Write a description of class MyHeap here.
@@ -16,10 +17,13 @@ public class MyHeap<E extends Comparable<E>>
     /**
      * Constructor for objects of class MyHeap
      */
-    public MyHeap(int maxSize) {
+    public MyHeap(boolean isMinHeap) {
         count = 0;
-        isMinHeap = 1;
-        heap = (E[]) new Comparable[maxSize];
+        heap = (E[]) new Comparable[1];
+        this.isMinHeap = 1;
+        if(isMinHeap){
+            this.isMinHeap = -1;
+        }
     }
     
     /**
@@ -27,49 +31,61 @@ public class MyHeap<E extends Comparable<E>>
      */
     public void add(E element) {
         int child = count;
-        int parent = (child-1)/2;
+        int parent = (child - 1) / 2;
         
-        while(child != 0 && element.compareTo(heap[parent])>0) {
+        while(child != 0 && isMinHeap * element.compareTo(heap[parent])>0) {
             heap[child] = heap[parent];
             child = parent;
-            parent = (child-1)/2;
+            parent = (child - 1) / 2;
         }
         
         heap[child] = element;
+        heap = Arrays.copyOf(heap, heap.length * 2);
         count++;
     }
     
     /**
      * Constructor for objects of class MyHeap
      */
-    public E getMax() {
+    public E getTop() {
         return heap[0];
     }
     
-    public E removeMax() {
+    public E removeTop() {
         if(isEmpty()){
             throw new NoSuchElementException();
         } else {
-            int parent = 0;
-            int child;
+            E top = heap[0];
             int idx = 0;
-            int max = heap[0];
             heap[idx] = heap[count-1];
-            count--;
-            while (idx*2+1<count && heap[idx*2+1]!=0) {
+            
+            while (idx*2+1<count) {
+                int left = idx*2+1;
+                int right = idx*2+2;
                 
-                if (heap[idx] > heap[idx*2+1] && heap[idx] > heap[idx*2+2]){
+                if (heap[idx].compareTo(heap[idx*2+1]) == isMinHeap 
+                        && heap[idx].compareTo(heap[idx*2+2]) == isMinHeap){
                     
-                } else if (heap[idx*2+1] > heap[idx*2+2]) {
-                    heap[idx] = heap[idx*2+1];
-                    idx = e;
+                } else if (heap[idx*2+1].compareTo(heap[idx*2+2]) == isMinHeap) {
+                    E temp = heap[idx];
+                    heap[idx] = heap[left];
+                    heap[left] = temp;
+                    idx = left;
+                } else {
+                    E temp = heap[idx];
+                    heap[idx] = heap[right];
+                    heap[right] = temp;
+                    idx = right;
                 }
             }
-            return null;
+            count--;
+            return top;
         }
     }
     
-    public boolean isEmpty
+    public boolean isEmpty() {
+        return count == 0;
+    }
     
     /**
      * Constructor for objects of class MyHeap
