2b68084fa5d31f7f0b4ea5e810089589555d2074
Newton Zheng <newtonzheng2009@gmail.com>
2026-02-25 13:15:15 -0800

    Changes

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,4 +1,4 @@
-
+import java.lang.Math;
 /**
  * Write a description of class MyHashTable here.
  *
@@ -14,20 +14,45 @@ public class MyHashTable
     /**
      * Constructor for objects of class MyHashTable
      */
-    public MyHashTable()
-    {
+    public MyHashTable() {
         table = new String[10];
         count = 0;
     }
     
     public void put(String key, String value) {
-        
+        table[getHash(key)] = value;
     }
     
-    private int getHash(String key){
-        return key.hashCode() % count;
+    public boolean isEmpty() {
+        return count == 0;
     }
     
+    public int size() {
+        return count;
+    }
+    
+    public String get(String key) {
+        return table[getHash(key)];
+    }
+    
+    private int getHash(String key) {
+        int hash = Math.abs(key.hashCode());
+        return hash % table.length;
+    }
+    
+    public String remove(String key) {
+        String str = table[getHash(key)];
+        table[getHash(key)] = null;
+        return str;
+    }
+    
+    public String toString() {
+        String hashTable = table[0] + "";
+        for(int i = 1; i < count; i++) {
+            hashTable += "," + table[i];
+        }
+        return hashTable;
+    }
     /**
      * An example of a method - replace this comment with your own
      *
