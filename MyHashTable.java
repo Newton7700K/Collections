7a8f416b613bfbc502dc6355986b56c9b7bc1806
Newton Zheng <newtonzheng2009@gmail.com>
2026-02-27 09:10:44 -0800

    Changes

--- MyHashTable.java
+++ MyHashTable.java
@@ -1,26 +1,27 @@
 import java.lang.Math;
+import java.util.Arrays;
 /**
  * Write a description of class MyHashTable here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyHashTable
+public class MyHashTable<K,V>
 {
     
-    private String[] table;
+    private Node[] table;
     private int count;
     
     /**
      * Constructor for objects of class MyHashTable
      */
     public MyHashTable() {
-        table = new String[10];
+        table = new Node[10];
         count = 0;
     }
     
-    public void put(String key, String value) {
-        table[getHash(key)] = value;
+    public void put(K key, V value) {
+        addToBucket(getHash(key), new Node(key, value));
     }
     
     public boolean isEmpty() {
@@ -31,32 +32,87 @@ public class MyHashTable
         return count;
     }
     
-    public String get(String key) {
-        return table[getHash(key)];
+    public V get(K key) {
+        return (V) table[getHash(key)].getValue();
     }
     
-    private int getHash(String key) {
+    private int getHash(K key) {
         int hash = Math.abs(key.hashCode());
         return hash % table.length;
     }
     
-    public String remove(String key) {
-        String str = table[getHash(key)];
+    public Node remove(K key) {
+        Node node = table[getHash(key)];
         table[getHash(key)] = null;
-        return str;
+        return node;
+    }
+    
+    public void addToBucket(int bucket, Node newNode){
+        newNode.setNext(table[bucket]);
+        table[bucket] = newNode;
+    }
+    
+    public Node searchBucket(int bucket, K key){
+        Node currentNode = table[bucket];
+        while(currentNode != null) {
+            if(currentNode.getKey().equals(key)){
+                return currentNode;
+            }
+        }
+        return null;
     }
     
     public String toString() {
-        String hashTable = table[0] + "";
-        for(int i = 1; i < count; i++) {
-            hashTable += "," + table[i];
-        }
-        return hashTable;
+        return Arrays.toString(table);
     }
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
+    
+    public class Node<K,V> {
+        private K key;
+        private V value;
+        private Node next;
+        
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+        private K getKey(){
+            return key;
+        }
+        
+        private V getValue(){
+            return value;
+        }
+        
+        private void setKey(K key) {
+            this.key = key;
+        }
+        
+        private void setValue(V value) {
+            this.value = value;
+        }
+        
+        private void setNext(Node next) {
+            this.next = next;
+        }
+
+        private Node getNext() {
+            return next;
+        }
+        
+        private void clear() {
+            value = null;
+            next = null;
+        }
+        
+        public String toString() {
+            String str = key + "->" + value;
+            Node nextNode = next;
+            while(nextNode != null){
+                str += "|" + nextNode.getKey() + "->" + nextNode.getValue();
+                nextNode = nextNode.getNext();
+            }
+            return str;
+        }
+    }
 }
