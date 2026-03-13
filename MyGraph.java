8cd6a313558bd1e41779900787367a057d1cc70b
Newton Zheng <newtonzheng2009@gmail.com>
2026-03-13 09:10:29 -0700

    fsfd

--- MyGraph.java
+++ MyGraph.java
@@ -11,23 +11,46 @@ public class MyGraph
 {
     private MyHashTable<String, Vertex> vertices;
     
-    public MyGraph()
-    {
+    public MyGraph() {
        vertices = new MyHashTable<String, Vertex>();
     }    
     
     public void addVertex(String label) {
+        if(label == null){
+            throw new NullPointerException();
+        }
         
+        if(vertices.get(label) != null){
+            throw new IllegalArgumentException();
+        }
+        
+        vertices.put(label, new Vertex(label));
+    }
+    
+    public void addEdge(String fromLabel,  String toLabel) {
+        if(fromLabel == null || toLabel == null) {
+            throw new NullPointerException();
+        }
+        
+        if(vertices.get(fromLabel) == null || vertices.get(toLabel) == null) {
+            throw new NoSuchElementException();
+        }
     }
     
     public class Vertex {
         private String label;
         private ArrayList<Vertex> neighbors = new ArrayList<>();
 
-        private Vertex(String label){
+        private Vertex(String label) {
             this.label = label;
         }
         
-        
+        public String toString() {
+            String result = label + ":";
+            for(int i = 0; i<neighbors.size(); i++){
+                result += "[" + neighbors.get(i).label + "]";
+            }
+            return result;
+        }
     }
 }
